108678e936d8efe4b37c57e7fb62d48a4c625001
CaptDR <weirdox911@gmail.com>
2024-12-09 11:53:42 +0700

    Pertemuan 12

--- PBO/Pertemuan 10/Latihan1/main.py
+++ PBO/Pertemuan 10/Latihan1/main.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 class HelloWorld(BoxLayout):
     pass
 class HelloWorldApp(App):
     def build (self):
+        Builder.load_file('main.kv')
         return HelloWorld()
 if __name__ == '__main__':
     HelloWorldApp().run()
