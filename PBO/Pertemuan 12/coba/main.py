2d097db58188a125fb3e6e324c9d6bc68e160517
CaptDR <weirdox911@gmail.com>
2024-12-12 19:08:25 +0700

    Pertemuan 12

--- PBO/Pertemuan 12/coba/main.py
+++ PBO/Pertemuan 12/coba/main.py
@@ -40,6 +40,8 @@ class InputForm(BoxLayout):
         )
 
         # Tambahkan tombol untuk menutup pop-up
+        content = BoxLayout(orientation='vertical')
+        content.add_widget(Label(text=data))
         close_btn = Button(text="Tutup", size_hint_y=None, height=40)
         close_btn.bind(on_release=popup.dismiss)
         popup.content.add_widget(close_btn)
@@ -49,37 +51,44 @@ class InputForm(BoxLayout):
         popup.open()
         # Membuat Koneksi Ke Database
 
-        sql = "INSERT INTO tbl_mahasiswa VALUES (%s, %s, %s)"
+        sql = "INSERT INTO tbl_mahasiswa (nama, nim, jurusan) VALUES (%s, %s, %s)"
         val = (nama, nim, jurusan)
-        self.mycursor.execute(sql, val)
-        self.mydb.commit()
+        try:
+            self.mycursor.execute(sql, val)
+            self.mydb.commit()
+            print("Data Berhasil Disimpan!")
+        except Exception as e:
+            print(f"Terjadi Kesalahan: {e}")
+    
         
         # Bersihkan text input
         self.ids.nama_input.text = ""
         self.ids.nim_input.text = ""
         self.ids.jurusan_input.text = ""
-        self.show_Table()
-    def show_table(self):
-        self.mycursor.execute("SELECT * FROM tbl_mahasiswa")
-        myresult = self.mycursor.fetchall()
-        self.ids.table.clear_widgets()
-        self.ids.table.add_widget(Label(text="Data Mahasiswa"))
-        self.ids.tabel_mahasiswa.add_widget(Label(text="Nama", bold=True))
-        self.ids.tabel_mahasiswa.add_widget(Label(text="Nim", bold=True))
-        self.ids.tabel_mahasiswa.add_widget(Label(text="Jurusan", bold=True))
+        self.show_table()
         
-        for i in myresult:
-            for item in i:
-                self.ids.tabel_mahasiswa.add_widget(Label(text=item))
+    def show_table(self):
+        self.mycursor.execute("SELECT nama, nim, jurusan FROM tbl_mahasiswa")
+        data_mahasiswa = self.mycursor.fetchall()
+        self.ids.tabel_mahasiswa.clear_widgets()
+        self.ids.tabel_mahasiswa.add_widget(Label(text="Nama", bold=True, size_hint_x=None, width=140))
+        self.ids.tabel_mahasiswa.add_widget(Label(text="Nim", bold=True, size_hint_x=None, width=140))
+        self.ids.tabel_mahasiswa.add_widget(Label(text="Jurusan", bold=True, size_hint_x=None, width=140))
+        
+        for row in data_mahasiswa:
+            for item in row:
+                self.ids.tabel_mahasiswa.add_widget(Label(text=item, size_hint_x=None, width=140))
+    
+    def on_close(self):
+        self.mycursor.close()
+        self.mydb.close()
         
 class form(App):
     def build(self):
         self.get_color_from_hex = get_color_from_hex # Utility Warna
         self.root_widget = InputForm()
         return self.root_widget
-    def on_close(self):
-        self.mydb.close()
-        self.mycursor.close()
+
     def on_start(self):
         Window.size = (600, 600)
         self.title = "Formulir Pendaftaran Mahasiswa"
